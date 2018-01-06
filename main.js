34f208e94c741ca3cf732d6e413f61a7a784c04a
skysthelimit91 <skylerdavisny@gmail.com>
2018-01-06 13:15:06 -0500

    pushing updates to video player before I most likely breaky everything

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var button1Location = document.getElementById('button1');
 var button2Location = document.getElementById('button2');
+var allBuutons = document.getElementsByClassName('play');
 var vid1Location = document.getElementById('vid1');
 var vid2Location = document.getElementById('vid2');
 var tvLocation = document.getElementById('tv');
+var slide1Location = document.getElementById('slide1');
+var slide2Location = document.getElementById('slide2');
 
 button1Location.addEventListener('click', playGF2);
 
@@ -10,6 +13,8 @@ function playGF2() {
   tvLocation.style.background = 'black';
   vid1Location.style.opacity = 1;
   vid1Location.play();
+  slide2Location.style.display = 'none';
+  slide1Location.style.display = 'block';
   button2Location.removeEventListener('click', playGT);
 }
 
@@ -19,5 +24,7 @@ function playGT() {
   tvLocation.style.background = 'black';
   vid2Location.style.opacity = 1;
   vid2Location.play();
+  slide1Location.style.display = 'none';
+  slide2Location.style.display = 'block';
   button1Location.removeEventListener('click', playGF2);
 }
